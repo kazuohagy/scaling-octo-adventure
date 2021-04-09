45293ae85336e646f1383ad2dc7cc76b7dff313e
Japaranha <kazucalado@gmail.com>
2021-04-09 16:31:22 -0300

    simplified code

--- src/application/Program1.java
+++ src/application/Program1.java
@@ -8,12 +8,9 @@ import java.io.IOException;
 public class Program1 {
 	public static void main(String[] args) {
 		File path = new File("c:\\teste\\in.txt");
-		FileReader fr = null;
-		BufferedReader br = null;
 
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
 
 			String line = br.readLine();
 			while (line != null) {
@@ -22,18 +19,7 @@ public class Program1 {
 			}
 
 		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			System.out.println(e.getMessage()); 
 		}
 	}
 }
